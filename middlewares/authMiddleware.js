9878b0eeddd9f3e33fef256732667926b8f20f38
TiTa05 <stevensmex05@gmail.com>
2025-03-18 21:40:13 +0300

    Upload picture
    implementation of a photo storage system for the profile picture

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,21 +1,21 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const authMiddleware = (roles = []) => {
   return (req, res, next) => {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ message: 'Accès refusé' });
+    const token = req.header("Authorization");
+    if (!token) return res.status(401).json({ message: "Accès refusé" });
 
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = decoded;
 
       if (roles.length && !roles.includes(req.user.type)) {
-        return res.status(403).json({ message: 'Accès interdit' });
+        return res.status(403).json({ message: "Accès interdit" });
       }
 
       next();
     } catch (error) {
-      res.status(401).json({ message: 'Token invalide' });
+      res.status(401).json({ message: "Token invalide" });
     }
   };
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,93 +1,128 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-const authMiddleware = require('../middlewares/authMiddleware');
-const multer = require('multer');
-const upload = multer();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const authMiddleware = require("../middlewares/authMiddleware");
+const { upload, uploadPath } = require("../middlewares/multerMiddleware");
+const fs = require("fs");
+const path = require("path");
+const mongoose = require("mongoose");
 
 // Inscription
 
-router.post('/client', upload.single('picture'), async (req, res) => {
-    try {
-        const { firstName, lastName, phoneNumber, email, password } = req.body;
-        const picture = req.file;
+router.post("/client", upload.single("picture"), async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
 
-        if (!password) {
-            return res.status(400).json({ message: 'Password is required' });
-        }
+  try {
+    const { firstName, lastName, phoneNumber, email, password } = req.body;
 
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const user = new User({
-            firstName,
-            lastName,
-            phoneNumber,
-            email,
-            password: hashedPassword,
-            picture: picture ? picture.buffer : null,
-            type: 1
-        });
-
-        await user.save();
-        res.status(201).json({ message: 'User created successfully', user });
-    } catch (error) {
-        console.error('Erreur :', error);
-        res.status(500).json({ message: 'Server error' });
+    if (!password) {
+      return res.status(400).json({ message: "Password is required" });
     }
+
+    let user = await User.findOne({ $or: [{ email }, { phoneNumber }] });
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "Email or phone number already used" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = new User({
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+      password: hashedPassword,
+      picture: null,
+      type: 1,
+    });
+
+    user = await user.save({ session });
+
+    if (req.file) {
+      const extension = path.extname(req.file.originalname);
+      const newFileName = `${user._id}${extension}`;
+      const oldPath = req.file.path;
+      const newPath = path.join(uploadPath, newFileName);
+
+      fs.renameSync(oldPath, newPath);
+
+      user.picture = `assets/pictures/${newFileName}`;
+      await user.save({ session });
+    }
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-
-router.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-
-        // Vérifier si l'utilisateur existe
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(400).json({ message: 'Email ou mot de passe invalide' });
-        }
-
-        // Vérifier si user.password est bien une string
-        if (typeof user.password !== "string") {
-            return res.status(500).json({ message: "Erreur serveur : problème avec le hash du mot de passe" });
-        }
-
-        // Vérifier le mot de passe
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ message: 'Email ou mot de passe invalide' });
-        }
-
-        // Générer un token JWT
-        const token = jwt.sign(
-            { id: user._id, type: user.type },
-            process.env.JWT_SECRET,
-            { expiresIn: process.env.JWT_EXPIRATION || '1h' }
-        );
-
-        res.json({ token, user: { id: user._id, email: user.email, type: user.type } });
-
-    } catch (error) {
-        res.status(500).json({ message: 'Erreur serveur' });
+    // Vérifier si l'utilisateur existe
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Email ou mot de passe invalide" });
     }
+
+    // Vérifier si user.password est bien une string
+    if (typeof user.password !== "string") {
+      return res.status(500).json({
+        message: "Erreur serveur : problème avec le hash du mot de passe",
+      });
+    }
+
+    // Vérifier le mot de passe
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ message: "Email ou mot de passe invalide" });
+    }
+
+    // Générer un token JWT
+    const token = jwt.sign(
+      { id: user._id, type: user.type },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRATION || "1h" }
+    );
+
+    res.json({
+      token,
+      user: { id: user._id, email: user.email, type: user.type },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Erreur serveur" });
+  }
 });
 
 // Route protégée pour le manager
-router.get('/admin', authMiddleware([3]), (req, res) => {
-    res.json({ message: 'Bienvenue, manager !' });
+router.get("/admin", authMiddleware([3]), (req, res) => {
+  res.json({ message: "Bienvenue, manager !" });
 });
 
 // Route protégée pour le mecano
-router.get('/mecanicien', authMiddleware([2]), (req, res) => {
-    res.json({ message: 'Espace mécanicien !' });
+router.get("/mecanicien", authMiddleware([2]), (req, res) => {
+  res.json({ message: "Espace mécanicien !" });
 });
 
 // Route protégée pour le client
-router.get('/client', authMiddleware([1]), (req, res) => {
-    res.json({ message: 'Bienvenue à vous !' });
+router.get("/client", authMiddleware([1]), (req, res) => {
+  res.json({ message: "Bienvenue à vous !" });
 });
 
 module.exports = router;
